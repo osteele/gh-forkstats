94eab8057cea1eee3ae76d4e43ebf3f07671e296
Oliver Steele <steele@osteele.com>
2021-09-20 16:02:38 +0800

    Port to TypeScript

---
+++ ambient.d.ts
@@ -0,0 +1,2 @@
+declare module 'table';
+declare module 'relative-date';
--- index.test.ts
+++ index.test.ts
@@ -2,12 +2,16 @@ import { comparator } from './index';
 
 test('comparator runs projection function', () => {
     const lst = [{ k: 1 }, { k: 3 }, { k: 2 }];
-    lst.sort(comparator(o => o.k))
+    lst.sort(comparator(o => o.k));
     expect(lst.map(({ k }) => k)).toEqual([1, 2, 3]);
 });
 
 test('comparator follows totalCount', () => {
-    const lst = [{ k: { totalCount: 1 } }, { k: { totalCount: 3 } }, { k: { totalCount: 2 } }];
-    lst.sort(comparator(o => o.k))
+    const lst = [
+        { k: { totalCount: 1 } },
+        { k: { totalCount: 3 } },
+        { k: { totalCount: 2 } },
+    ];
+    lst.sort(comparator(o => o.k));
     expect(lst.map(({ k }) => k.totalCount)).toEqual([1, 2, 3]);
 });
